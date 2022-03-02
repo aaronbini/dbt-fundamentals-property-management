0a8dc3baf3a05b4bb1c5073c0c1f61087b3cf96b
Aaron Bini <bini.aaron@gmail.com>
2022-03-02 00:35:54 +0000

    working on dim_properties

--- models/marts/core/dim_properties.sql
+++ models/marts/core/dim_properties.sql
@@ -10,15 +10,23 @@ units as (
     select * from {{ ref('stg_units') }}
 ),
 
+grouped_units as (
+    select
+        property_id,
+        count(unit_id) as number_of_units
+    from units
+    group by property_id
+),
+
 final as (
     select
         property_id,
         property_owners.property_owner_id,
-        count(units.*) as num_units
+        grouped_units.number_of_units
     from properties
     join property_owners using (property_id)
-    join units using (property_id)
-    group by property_id
+    join grouped_units using (property_id)
+    group by 1
 )
 
 select * from final
